68ae1788064e9b83cefa4ac585e9ed3ce0b8630e
BuiBao3103 <hongbao2003@gmail.com>
2023-07-22 17:00:36 +0700

    move-zeroes

---
+++ move-zeroes.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let step = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) step++
+        else if (step > 0) {
+            nums[i - step] = nums[i]
+            nums[i] = 0
+        }
+    }
+};
