8441a06193053cfc93a5e9865dd44cb6cc6d4ef9
BuiBao3103 <hongbao2003@gmail.com>
2023-07-25 13:54:43 +0700

    longest-substring-without-repeating-characters

---
+++ longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let set = new Set()
+    let maxLength = 0
+    let left = 0
+    if (s.length == 0) return 0
+    if (s.length == 1) return 1
+    for (let i = 0; i < s.length; i++) {
+        while (set.has(s[i])) {
+            set.delete(s[left])
+            left++
+        }
+        set.add(s[i])
+        maxLength = Math.max(maxLength, i - left + 1)
+    }
+    return maxLength
+};
