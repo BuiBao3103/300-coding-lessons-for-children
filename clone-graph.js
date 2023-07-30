86b254fd783bd6917c117268f39ee70d3798e1c2
BuiBao3103 <hongbao2003@gmail.com>
2023-07-30 23:58:15 +0700

    clone-graph

---
+++ clone-graph.js
@@ -0,0 +1,22 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function (node, copy = new Map()) {
+    if (!node) return null;
+
+    if (!copy.has(node.val)) {
+        copy.set(node.val, new Node(node.val));
+        copy.get(node.val).neighbors = node.neighbors.map((neighbor) => cloneGraph(neighbor, copy));
+    }
+
+    return copy.get(node.val);
+};
